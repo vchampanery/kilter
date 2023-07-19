e0075986c1332b9e843ab062f776ad52ab350614
demo strava <vchampanery@gmail.com>
2023-07-19 23:43:21 +0530

    test

--- resources/views/layouts/leftside.blade.php
+++ resources/views/layouts/leftside.blade.php
@@ -5,7 +5,7 @@
             <img src="{{asset('assets/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-            <a href="#" class="d-block">Virus</a>
+            <a href="#" class="d-block">Kilter Fun</a>
         </div>
     </div>
 
