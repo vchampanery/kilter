0e08faf9fba0cf6786a5b1dd1a28e5d52aece369
demo strava <vchampanery@gmail.com>
2023-07-28 01:31:00 +0530

    update

--- app/Http/Controllers/StravaController.php
+++ app/Http/Controllers/StravaController.php
@@ -98,8 +98,8 @@ class StravaController extends Controller
      * 
      */
     public function getdatabycron(){
-        // $userObj = stravauserauth::where('user_id','>' ,122)->orderBy('user_id', 'asc')->get(['user_id']);
-        $userObj = stravauserauth::orderBy('user_id', 'asc')->get(['user_id']);
+        $userObj = stravauserauth::where('user_id','>' ,131)->orderBy('user_id', 'asc')->get(['user_id']);
+        // $userObj = stravauserauth::orderBy('user_id', 'asc')->get(['user_id']);
         foreach($userObj as $key=>$val){
             dump("started for".$val->user_id.' at '.time());    
 
