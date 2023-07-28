7aad701dbe1cc244b8cb4a4fd103a7b1f52a2e51
demo strava <vchampanery@gmail.com>
2023-07-28 20:50:24 +0530

    add cron betweenlimit

--- app/Http/Controllers/StravaController.php
+++ app/Http/Controllers/StravaController.php
@@ -97,9 +97,9 @@ class StravaController extends Controller
     /**
      * 
      */
-    public function getdatabycron(){
-        // $userObj = stravauserauth::where('user_id','>' ,131)->orderBy('user_id', 'asc')->get(['user_id']);
-        $userObj = stravauserauth::orderBy('user_id', 'asc')->get(['user_id']);
+    public function getdatabycron($start,$end){
+        $userObj = stravauserauth::whereBetween('user_id',[$start,$end])->orderBy('user_id', 'asc')->get(['user_id']);
+        // $userObj = stravauserauth::orderBy('user_id', 'asc')->get(['user_id']);
         foreach($userObj as $key=>$val){
             dump($val->user_id);
             // echo $val->user_id." , ";    
