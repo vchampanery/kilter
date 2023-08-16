d731085769cca4e35bffff146643bbc3068231a3
demo strava <vchampanery@gmail.com>
2023-08-16 20:31:00 +0530

    asdfafd

--- app/Http/Controllers/StravaController.php
+++ app/Http/Controllers/StravaController.php
@@ -482,7 +482,7 @@ class StravaController extends Controller
         if($today){
             $page = 1;
             
-            $before  = strtotime("-15 day 00:00:00");
+            $before  = strtotime("-16 day 00:00:00");
             $after   = strtotime("today 23:59:59");
             dump($before);
             dump($after);
