832eca674d1ada69e66fa7c05c07744373791ea5
demo strava <vchampanery@gmail.com>
2023-09-04 21:11:36 +0530

    update for the board

--- resources/views/team_life_board.blade.php
+++ resources/views/team_life_board.blade.php
@@ -9,7 +9,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">All time Dashboard</li>
+              <li class="breadcrumb-item active">All time Dashboard</li> 
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
