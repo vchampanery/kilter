fbd4a3c26a465629622f89cb51b9cc4dbd349ff1
demo strava <vchampanery@gmail.com>
2023-09-18 18:48:43 +0530

    update

--- resources/views/hometest.blade.php
+++ resources/views/hometest.blade.php
@@ -15,6 +15,12 @@
      crossorigin="anonymous"></script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+     <!-- PWA  -->
+    <meta name="theme-color" content="#fffff"/>
+     <link rel="apple-touch-icon" href="{{ asset('assets/dist/img/AdminLTELogo.png') }}">
+     <link rel="manifest" href="{{ asset('/manifest.json') }}">
+
     <title>Kilter-Fun</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -59,6 +65,22 @@
 </div>
 <!-- ./wrapper -->
 
-
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+    if ("serviceWorker" in navigator) {
+        // Register a service worker hosted at the root of the
+        // site using the default scope.
+        navigator.serviceWorker.register("/sw.js").then(
+        (registration) => {
+            console.log("Service worker registration succeeded:", registration);
+        },
+        (error) => {
+            console.error(`Service worker registration failed: ${error}`);
+        },
+        );
+    } else {
+        console.error("Service workers are not supported.");
+    }
+    </script>
 </body>
 </html>
